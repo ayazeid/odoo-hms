3a4dc989fa4f81375da055383fb22e3e1b047898
ayazeid <aya.zeid95@gmail.com>
2022-02-14 21:30:55 +0200

    Add user groups secuirty access permissions and rules

--- hms/__manifest__.py
+++ hms/__manifest__.py
@@ -9,11 +9,15 @@
     """,
     'author': "Aya Zeid",
     'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/hms_patient_view.xml',
         'views/hms_doctor_view.xml',
         'views/hms_department_view.xml',
         'views/hms_customer_view.xml',
-        'views/hms_patientlog_view.xml'
+        'views/hms_patientlog_view.xml',
+        'reports/report.xml',
+        'reports/templates.xml'
 
     ],
 }
